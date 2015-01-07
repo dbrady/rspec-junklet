17e13d2aa47f05560cca0f0ecaed7f4a4708e0ed
Dave Brady <dbrady@covermymeds.com>
2015-01-06 21:26:08 -0700

    Multiple args to junk_let

--- lib/junklet.rb
+++ lib/junklet.rb
@@ -1,15 +1,13 @@
 require "junklet/version"
 
-module Junklet
-  # Your code goes here...
-end
-
 module RSpec
   module Core
     module MemoizedHelpers
       module ClassMethods
-        def junk_let(name)
-          let(name) { "#{name}-#{SecureRandom.uuid}" }
+        def junk_let(*args)
+          Array(args).each do |name|
+            let(name) { "#{name}-#{SecureRandom.uuid}" }
+          end
         end
       end
     end
--- spec/lib/junklet_spec.rb
+++ spec/lib/junklet_spec.rb
@@ -4,10 +4,31 @@ describe Junklet do
   specify { expect(Junklet).to be }
 
   let(:regex) { /-[\da-f]{8}-[\da-f]{4}-[\da-f]{4}-[\da-f]{4}-[\da-f]{12}$/ }
-  
-  junk_let :junk
 
-  specify { expect(junk).to be }
-  specify { expect(junk).to match /^junk-/ }
-  specify { expect(junk).to match regex }
+  describe '.junk_let' do
+    context "with a single arg" do
+      junk_let :junk
+      
+      specify { expect(junk).to be }
+      specify { expect(junk).to match /^junk-/ }
+      specify { expect(junk).to match regex }
+      
+      describe "memoization" do
+        specify { expect(junk).to eq(junk) }
+      end
+    end
+
+    context "with multiple args" do
+      junk_let :junk, :toss, :crud, :crap
+
+      specify { expect(junk).to match /^junk-/ }
+      specify { expect(junk).to match regex }
+      specify { expect(toss).to match /^toss-/ }
+      specify { expect(toss).to match regex }
+      specify { expect(crud).to match /^crud-/ }
+      specify { expect(crud).to match regex }
+      specify { expect(crap).to match /^crap-/ }
+      specify { expect(crap).to match regex }
+    end
+  end
 end
