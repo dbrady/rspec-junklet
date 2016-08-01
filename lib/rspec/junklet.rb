7437a8453bfdfc8515629ee405df37d791305a77
David Brady <dbrady@shinybit.com>
2016-08-01 11:43:21 -0600

    Add comments to better document RSpec includes and extends

--- lib/rspec/junklet.rb
+++ lib/rspec/junklet.rb
@@ -2,8 +2,9 @@ require_relative "./junklet/version"
 require_relative "./junklet/junk"
 require_relative "./junklet/junklet"
 
+# Automatically hook into RSpec when you `include "rspec-junklet"`
 RSpec.configure do |config|
-  config.extend(RSpec::Junklet::Junklet)
-  config.extend(RSpec::Junklet::Junk) # when metaprogramming cases, you may need junk in ExampleGroups
-  config.include(RSpec::Junklet::Junk)
+  config.extend(RSpec::Junklet::Junklet) # This lets us say junklet() in describes and contexts
+  config.extend(RSpec::Junklet::Junk)    # This lets us say junk() in describes and contexts
+  config.include(RSpec::Junklet::Junk)   # This lets us say junk() in lets
 end
